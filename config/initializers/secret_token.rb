f20c7e40b5d03c1cdf3d6391c0cc02abc1b9ce47
Chris Oliver <excid3@gmail.com>
2013-10-21 20:51:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnimatedgifMe::Application.config.secret_key_base = 'a40a3aa9dde0ed48b040d01509913ef2dac7af3013b697c65b964ec5830a41af3896b711bc3d46fa867ad3183ff0af0188b5041c57a8d69da4c3c73f5a63562e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnimatedgifMe::Application.config.session_store :cookie_store, key: '_animatedgif_me_session'
