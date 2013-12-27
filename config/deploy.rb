6e729ede3cf2c1460f038e8f1a84a7f459932a17
Chris Oliver <excid3@gmail.com>
2013-12-26 21:28:20 -0600

    Restart after finishing deploy and remove clear_cache

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,15 +25,6 @@ namespace :deploy do
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
   after :finishing, 'deploy:cleanup'
-
+  after :finishing, 'deploy:restart'
 end
